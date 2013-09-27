277a197acff6e38a1fda93345b32ec2fc3ea3d08
Carmen Maria Lanz (Aquila) <elemmire@ossiriand.de>
2013-09-27 09:58:25 +0200

    content pages and retro layout

--- src/Papyrillio/HgvBundle/Controller/DefaultController.php
+++ src/Papyrillio/HgvBundle/Controller/DefaultController.php
@@ -4,12 +4,25 @@ namespace Papyrillio\HgvBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-
 class DefaultController extends Controller
 {
-    
-    public function indexAction($name)
-    {
-        return $this->render('PapyrillioHgvBundle:Default:index.html.twig', array('name' => $name));
-    }
+  public function indexAction(){
+    return $this->render('PapyrillioHgvBundle:Default:index.html.twig');
+  }
+
+  public function abbreviationAction(){
+    return $this->render('PapyrillioHgvBundle:Default:abbreviation.html.twig');
+  }
+
+  public function helpAction($topic = '', $language = ''){
+    return $this->render('PapyrillioHgvBundle:Default:help' . ucfirst($topic) . ucfirst($language) . '.html.twig');
+  }
+
+  public function introductionAction(){
+    return $this->render('PapyrillioHgvBundle:Default:introduction.html.twig');
+  }
+
+  public function publicationAction(){
+    return $this->render('PapyrillioHgvBundle:Default:publication.html.twig');
+  }
 }
