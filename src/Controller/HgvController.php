e626af80ba6b5f1cc2afc8fc6251faf7d342f593
Elor Oké (Piccolo) <elor.oke@adamsakte.de>
2020-11-20 19:01:37 +0100

    get request object the Symfony 5 way

--- src/Controller/HgvController.php
+++ src/Controller/HgvController.php
@@ -3,14 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class HgvController extends AbstractController{
+  protected $request;
+
+  public function __construct(RequestStack $requestStack)
+  {
+      $this->request = $requestStack->getCurrentRequest();
+  }
   protected function getParameter($key){
-    $get  = $this->getRequest()->query->get($key);
-    $post = $this->getRequest()->request->get($key);
-    
+    $get  = $this->request->query->get($key);
+    $post = $this->request->request->get($key);
+
     if($post && (is_array($post) || strlen(trim($post)))){
       return $post;
     }
@@ -19,11 +27,11 @@ class HgvController extends AbstractController{
   }
 
   protected function getSessionParameter($key){
-    return $this->getRequest()->getSession()->get($key);
+    return $this->request->getSession()->get($key);
   }
 
   protected function setSessionParameter($key, $value){
-    return $this->getRequest()->getSession()->set($key, $value);
+    return $this->request->getSession()->set($key, $value);
   }
 
 }
