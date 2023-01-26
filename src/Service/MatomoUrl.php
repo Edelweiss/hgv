c8823c0f2c42044aad6a936233afd5f2c17debff
Elemmire <elemmire@ossiriand.de>
2023-01-26 14:52:04 +0100

    matomo

--- src/Service/MatomoUrl.php
+++ src/Service/MatomoUrl.php
@@ -7,7 +7,7 @@ class MatomoUrl extends Matomo {
   const MODULE_API = 'API';
 
   public function cloud($module = self::MODULE_WIDGET){
-    $return $this->url . '?module=' . $module . '&action=iframe&disableLink=1&widget=1&moduleToWidgetize=UserCountry&actionToWidgetize=getCountry&period=month&date=january&viewDataTable=cloud&idSite=' . $this->site . '&token_auth=' . $this->token;
+    return $this->url . '?module=' . $module . '&action=iframe&disableLink=1&widget=1&moduleToWidgetize=UserCountry&actionToWidgetize=getCountry&period=month&date=january&viewDataTable=cloud&idSite=' . $this->site . '&token_auth=' . $this->token;
   }
 }
 
