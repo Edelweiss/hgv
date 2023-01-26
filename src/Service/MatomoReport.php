e07d3a11e1c4b73f2f8f78dbdb1fcea666783fa3
Elemmire <elemmire@ossiriand.de>
2023-01-26 13:22:43 +0100

    matomo

--- src/Service/MatomoReport.php
+++ src/Service/MatomoReport.php
@@ -13,7 +13,16 @@ class MatomoReport extends Matomo {
   public function htmlCloud(){
     //$this->configure($path, $file, $direction);
     $url = 'https://papy.zaw.uni-heidelberg.de/tomo/index.php?module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=UserCountry&actionToWidgetize=getCountry&idSite=1&period=day&date=yesterday&viewDataTable=graphPie&token_auth=d3657e451381a2c526d74b0532e05c25';
-    return file_get_contents($url);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    // Set so curl_exec returns the result instead of outputting it.
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    // Get the response and close the channel.
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return $responste;
   }
 }
 
