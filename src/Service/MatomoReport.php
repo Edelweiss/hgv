46d839906e06b66e627099ef3a07f8d526ab7c9f
Elemmire <elemmire@ossiriand.de>
2023-02-13 14:13:40 +0100

    matomo

--- src/Service/MatomoReport.php
+++ src/Service/MatomoReport.php
@@ -3,7 +3,7 @@
 namespace App\Service;
 
 class MatomoReport extends MatomoUrl {
-  protected function getHtml($url){
+  protected function getResponse($url){
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     // Set so curl_exec returns the result instead of outputting it.
@@ -11,15 +11,16 @@ class MatomoReport extends MatomoUrl {
     // Get the response and close the channel.
     $response = curl_exec($ch);
     curl_close($ch);
+    return $response;
   }
 
   public function getVisitors(){
     return $this->getApiUrl('VisitFrequency.get', 'range', '2023-01-01,2023-02-15');
-    return $this->getHtml($this->getApiUrl('VisitFrequency.get', 'range', '2023-01-01,2023-02-15'));
+    return $this->getResponse($this->getApiUrl('VisitFrequency.get', 'range', '2023-01-01,2023-02-15'));
   }
 
   public function getCloud(){
-    return $this->getHtml($this->getCloudWidgetUrl());
+    return $this->getResponse($this->getCloudWidgetUrl());
   }
 }
 
