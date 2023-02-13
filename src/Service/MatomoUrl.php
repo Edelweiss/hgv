90718470c96009fbdd83ae07778ff42c97e94c36
Elemmire <elemmire@ossiriand.de>
2023-02-13 13:11:11 +0100

    matomo

--- src/Service/MatomoUrl.php
+++ src/Service/MatomoUrl.php
@@ -3,20 +3,33 @@
 namespace App\Service;
 
 class MatomoUrl extends Matomo {
-  const MODULE_WIDGET = 'Widgetize';
+  const MODULE_WIDGET = 'Widgetize&action=iframe&disableLink=1&widget=1';
   const MODULE_API = 'API';
   const FORMAT_JSON = 'JSON';
-
-  public function visitors($module = self::MODULE_API){
-    return $this->getApi('VisitFrequency.get', 'range', '2023-01-01,2023-02-15');
+  
+  private function getUrl($parameters){
+    return $this->url . $parameters . '&idSite=' . $this->site . '&token_auth=' . $this->token;
   }
 
-  public function getApi($method, $period, $date){
-    return $this->url . '?module=' . self::MODULE_API . '&format=' . self::FORMAT_JSON . '&method=' . $method . '&period=' . $period . '&date=' . $date . '&idSite=' . $this->site . '&token_auth=' . $this->token;
+  public function getApiUrl($method, $period, $date){
+    return $this->getUrl('?module=' . self::MODULE_API . '&format=' . self::FORMAT_JSON . '&method=' . $method . '&period=' . $period . '&date=' . $date);
   }
 
-  public function cloud($module = self::MODULE_WIDGET){
-    return $this->url . '?module=' . $module . '&action=iframe&disableLink=1&widget=1&moduleToWidgetize=UserCountry&actionToWidgetize=getCountry&period=range&date=2023-01-01,2023-02-15&viewDataTable=cloud&filter_limit=42&idSite=' . $this->site . '&token_auth=' . $this->token;
+  public function getWidgetUrl($widget, $action, $period, $date, $extra = ''){
+    return $this->url . '?module=' . self::MODULE_WIDGET . '&moduleToWidgetize' . $widget . '&actionToWidgetize' . $action . '&period=' . $period . '&date=' . $date . $extra . '&idSite=' . $this->site . '&token_auth=' . $this->token;
+  }
+
+  // API
+
+  public function getVisitorApiUrl(){
+    return $this->getApiUrl('VisitFrequency.get', 'range', '2023-01-01,2023-02-15');
+  }
+
+  // WIDGET
+
+  public function getCloudWidgetUrl(){
+    return this->getWidgetUrl('UserCountry', 'getCountry', 'range', '2023-01-01,2023-02-15', '&viewDataTable=cloud&filter_limit=42');
+    //return $this->url . '?module=' . $module . '&action=iframe&disableLink=1&widget=1&moduleToWidgetize=UserCountry&actionToWidgetize=getCountry&period=range&date=2023-01-01,2023-02-15&viewDataTable=cloud&filter_limit=42&idSite=' . $this->site . '&token_auth=' . $this->token;
   }
 }
 
