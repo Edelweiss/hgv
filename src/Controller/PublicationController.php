d4081e30f6323548e69ddee610c46664c7f23bd2
Elemmire <ul122@uni-heidelberg.de>
2023-06-22 13:13:39 +0200

    controller stub for Matomo report

---
+++ src/Controller/PublicationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Hgv;
+use App\Entity\Publication;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Service\MatomoReport;
+
+class PublicationController extends HgvController
+{
+
+}
